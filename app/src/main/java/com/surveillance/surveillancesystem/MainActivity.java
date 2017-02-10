18de0c39125294061e65579e499b74b89e3c0610
mikec22 <chan.xuan222@gmail.com>
2017-02-10 14:23:02 +0800

    nth

--- app/src/main/java/com/surveillance/surveillancesystem/MainActivity.java
+++ app/src/main/java/com/surveillance/surveillancesystem/MainActivity.java
@@ -1,5 +1,6 @@
 package com.surveillance.surveillancesystem;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -81,21 +82,16 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+
         } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+            Intent intent = new Intent(this, VideoDisplayActivity.class);
+            startActivity(intent);
         }
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
 }
---
+++ app/src/main/java/com/surveillance/surveillancesystem/VideoDisplayActivity.java
@@ -0,0 +1,68 @@
+package com.surveillance.surveillancesystem;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.vr.sdk.widgets.video.VrVideoView.Options;
+import com.google.vr.sdk.widgets.video.VrVideoView;
+import com.google.vr.sdk.widgets.video.VrVideoEventListener;
+
+import java.io.IOException;
+
+
+public class VideoDisplayActivity extends Activity {
+
+    /**
+     * Configuration information for the video
+     **/
+    private Options videoOptions = new Options();
+
+    private VideoLoaderTask videoLoaderTask;
+
+    private VrVideoView vrVideoView;
+    private Uri fileUri;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_display);
+        vrVideoView = (VrVideoView) findViewById(R.id.videoView);
+        vrVideoView.setEventListener(new VrVideoEventListener());
+        handleIntent(getIntent());
+    }
+
+    private void handleIntent(Intent intent) {
+        videoLoaderTask = new VideoLoaderTask();
+        videoLoaderTask.execute();
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        vrVideoView.pauseRendering();
+    }
+
+    class VideoLoaderTask extends AsyncTask<Void, Void, Boolean> {
+
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            try {
+                Options options = new Options();
+                options.inputType = Options.TYPE_MONO;
+                vrVideoView.loadVideoFromAsset("output.mp4", options);
+                Toast.makeText(VideoDisplayActivity.this, "Loaded.", Toast.LENGTH_LONG).show();
+            }catch (IOException e){
+                Toast.makeText(VideoDisplayActivity.this, "Cannot Open file.", Toast.LENGTH_LONG).show();
+            }
+            return true;
+        }
+    }
+}
